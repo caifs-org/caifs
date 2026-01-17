8ea8565059e65d569ea330f2cba9c0f80c032f68
rob miller <funky.style@gmail.com>
2026-01-17 10:48:17 +0800

    modify install script

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 INSTALL_PREFIX=${INSTALL_PREFIX:=$HOME/.local/}
-curl -sL https://github.com/vasdee/caifs/releases/download/v0.0.1/release.tar.gz | tar zxf -
+LATEST_VERSION=$(curl -sL https://api.github.com/repos/vasdee/caifs/releases/latest | grep '"tag_name":' | sed -E 's/.*"v?([^"]+)".*/\1/')
+
+curl -sL https://github.com/vasdee/caifs/releases/download/v$LATEST_VERSION/release.tar.gz | tar zxf -
 
 cp bin/caifs $INSTALL_PREFIX/bin/caifs
-cp lib/caifs-lib.sh $INSTALL_PREFIX/lib/caifs-lib.sh
+cp lib/caifs-lib.sh $INSTALL_PREFIX/lib/caifslib.sh
 
 chmod +x $INSTALL_PREFIX/bin/caifs
-chmod +x $INSTALL_PREFIX/lib/caifs-lib.sh
+chmod +x $INSTALL_PREFIX/lib/caifslib.sh
