cd266f8dd1930e1c4a12285363dbea8da94f224d
rob miller <funky.style@gmail.com>
2026-05-14 14:26:59 +0800

    fix: caifs-common install

--- caifs-common/hooks/pre.sh
+++ caifs-common/hooks/pre.sh
@@ -5,8 +5,8 @@ generic() {
     VERSION=${CAIFS_COMMON_VERSION:-$LATEST_VERSION}
 
     curl -sL https://github.com/caifs-org/caifs-common/releases/download/v"${VERSION}"/release.tar.gz | tar -xzf -
-    mkdir -p share/caifs-collections
-    mv caifs-common share/caifs-collections/
-    
-    caifs_install "share" 
+    mkdir -p "${CAIFS_INSTALL_DIR}"/share/caifs-collections
+    mv caifs-common "${CAIFS_INSTALL_DIR}"/share/caifs-collections/
+
+    caifs_install
 }
