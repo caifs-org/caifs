65da3770b0d2f94e62b5c0e839889846ad486a55
rob miller <funky.style@gmail.com>
2026-05-28 09:34:56 +0800

    fix: proper copy script

--- caifs/hooks/pre.sh
+++ caifs/hooks/pre.sh
@@ -1,7 +1,8 @@
 
 
 generic() {
-    cp -r ../../src/* "${CAIFS_INSTALL_DIR}"/
+    # escape the TMPDIR, Target dir, root dir
+    cp -r ../../../src/* "${CAIFS_INSTALL_DIR}"/
     chmod +x "${CAIFS_INSTALL_DIR}"/bin/caifs
     caifs_install
 }
