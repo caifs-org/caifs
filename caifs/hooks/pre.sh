6adb6c633ce36c5ab573f6ab2511b188f4eeb851
rob miller <funky.style@gmail.com>
2026-07-07 16:04:41 +0800

    fix: hooks install

--- caifs/hooks/pre.sh
+++ caifs/hooks/pre.sh
@@ -1,8 +1,9 @@
 
 
 generic() {
-    # escape the TMPDIR, Target dir, root dir
-    cp -r ../../../src/* "${CAIFS_INSTALL_DIR}"/
+    # escape the TMPDIR/HOOKS//Target dir/project dir
+    # I don't think I like this that much
+    cp -r ../../../../src/* "${CAIFS_INSTALL_DIR}"/
     chmod +x "${CAIFS_INSTALL_DIR}"/bin/caifs
     caifs_install
 }
