0d0c70f8b37d88edb9b9421e8d9c7935975e0062
rob miller <funky.style@gmail.com>
2026-08-07 14:24:40 +0800

    Bump version: 0.9.0 → 1.0.0

--- src/lib/caifs/utils.sh
+++ src/lib/caifs/utils.sh
@@ -31,7 +31,7 @@ log_error() {
 ## Some globals. These can generally be overridden via environment variables with the CAIFS_ prefix
 # By default, run both links and hooks
 # shellcheck disable=SC2034
-CAIFS_VERSION=0.9.0
+CAIFS_VERSION=1.0.0
 
 HOOKS_DIR=hooks
 
